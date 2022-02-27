ce2a372a8faf5e29db1e223bd098614f337047bd
Berry Liu <bl3056@nyu.edu>
2022-02-26 23:07:45 -0500

    Created API functionality

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 from flask import Flask, request, jsonify, session
 from Nutrition import autoFillFood, getCal
 from Exercise import calBurned
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+# from models import Users
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+app.secret_key = '1RUdd_QLhC69AVIJBeE-fA'
+app.config['SESSION_COOKIE_NAME'] = "API Cookie"
 
 class Users(db.Model):
     username = db.Column("username", db.String(100), primary_key=True)
     password = db.Column("password", db.String(100), nullable=False)
-    calGoal = db.Column("calGoal", db.Integer, nullable=False)
+    exGoal = db.Column("exGoal", db.Integer, nullable=False)
+    calLimit = db.Column("calLimit", db.Integer, nullable=False)
+    exCurr = db.Column("exCurr", db.Integer, default=0)
+    calCurr = db.Column("calCurr", db.Integer, default=0)
     gender = db.Column("gender", db.String(20))
     weight = db.Column("weight", db.Integer)
     height = db.Column("height", db.Integer)
     age = db.Column("age", db.Integer)
 
-    def __init__(self, username, password, calGoal, gender, weight, height, age):
+    def __init__(self, username, password, exGoal, calLimit, gender, weight, height, age):
         self.username = username
-        self.password = calGoal
-        self.calGoal = calGoal
+        self.password = password
+        self.exGoal = exGoal
+        self.calLimit = calLimit
+        self.exCurr = 0
+        self.calCurr = 0
         self.gender = gender
         self.weight = weight
         self.height = height
@@ -32,10 +42,48 @@ class Users(db.Model):
 # POST:
 # request.get_json(force=True)
 # store JSON in body
+
+
+
 @app.route('/signup', methods=['POST'])
 def signup():
-    
-    return
+    if request.method == 'POST':
+        username = request.form.get("username")
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            return {"result":"User already exists"}
+        password = request.form.get("password")
+        calLimit = request.form.get("calLimit")
+        exGoal = request.form.get("exGoal")
+        gender = request.form.get("gender")
+        weight = request.form.get("weight")
+        height = request.form.get("height")
+        age = request.form.get("age")
+        password = generate_password_hash(password, method="sha256")
+        newUser = Users(username, password, exGoal, calLimit, gender, weight, height, age)
+        db.session.add(newUser)
+        db.session.commit()
+        return {"result":"User has been added to database"}
+
+@app.route('/login', methods=['POST'])
+def login():
+    if session.get('logged_in'):
+        return {"result": "Already logged in"}
+    username = request.form.get("username")
+    password = request.form.get("password")
+    userMatch = Users.query.filter_by(username=username).first()
+    if not userMatch or not check_password_hash(userMatch.password, password):
+        return {"result": "Please check your login details and try again"}
+    session['logged_in'] = username
+    return {"result":"User has been logged in"}
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    if not session['logged_in']:
+        return {"result":"No user logged in"}
+    session['logged_in'] = None
+    return {"result":"Successfully logged out"}
+
 @app.route('/getFood', methods=['GET'])
 def getFood():
     """
@@ -49,32 +97,41 @@ def getFood():
         choices = autoFillFood(foodInput)
         return jsonify({'choices': choices})
 
-@app.route('/getCalories/<string:source>', methods=['GET'])
+@app.route('/getCalories/<string:source>', methods=['POST'])
 def getCalories(source):
     """
-    Takes in a JSON as params:
-    'food': string with the user selected food from the getFood function
-    'weight': weight of food in grams
-    :return:
-    JSON object with 'calories' mapped to the total calories consumed
+
     """
-    if request.method == "GET":
-        data = request.args
+    if request.method == "POST":
+        data = request.form
+        username = session['logged_in']
+        user = Users.query.filter_by(username=username).first()
         if source == "food":
             foodName = data.get('food')
             weight = int(data.get('weight'))
             calories = getCal(foodName, weight)
-            return jsonify({'calories': calories})
+            user.calCurr += calories
         elif source == "exercise":
-            #this data may be stored in database in the future
-            #instead, store logging in user in session and get their data from the database
             query = data.get('query')
-            gender = data.get('gender')
-            weight = data.get('weight')
-            height = data.get('height')
-            age = data.get('age')
+            gender = user.gender
+            weight = user.weight
+            height = user.height
+            age = user.age
             calories = calBurned(query,gender,weight,height,age)
-            return jsonify({'calories': calories})
+            user.exCurr += calories
+            over = user.calCurr - user.calLimit if user.calCurr > user.calLimit else 0
+            if user.exGoal - user.exCurr + over <= 0:
+                user.calCurr = 0
+                user.exCurr = 0
+                db.session.commit()
+                return {'done':True}
+        db.session.commit()
+        calLimit = user.calLimit
+        calCurr = user.calCurr
+        exCurr = user.exCurr
+        exGoal = user.exGoal
+        return jsonify({'done': False, 'calLimit':calLimit, 'calCurr':calCurr, 'exCurr':exCurr, 'exGoal':exGoal})
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
